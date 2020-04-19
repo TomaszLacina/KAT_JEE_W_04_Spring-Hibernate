c5990a72f0337342819b9afc31a9159498d8816c
TomekLacina <tla@avaleo.net>
2020-04-19 15:10:30 +0200

    dodanie join column do relacji

--- src/main/java/pl/coderslab/app/entity/Publisher.java
+++ src/main/java/pl/coderslab/app/entity/Publisher.java
@@ -15,6 +15,7 @@ public class Publisher {
     private String name;
 
     @OneToMany
+    @JoinColumn(name = "publisher_id")
     private List<Book> books = new ArrayList<>();
 
     public Long getId() {
