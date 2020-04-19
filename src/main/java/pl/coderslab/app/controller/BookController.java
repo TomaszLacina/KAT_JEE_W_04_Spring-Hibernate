484661aded61ea317831c8ee74ed0b871ce546de
TomekLacina <tla@avaleo.net>
2020-04-19 11:56:33 +0200

    Hibernate dzien 1, plus troche rest api

---
+++ src/main/java/pl/coderslab/app/controller/BookController.java
@@ -0,0 +1,55 @@
+package pl.coderslab.app.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import pl.coderslab.app.dao.BookDao;
+import pl.coderslab.app.entity.Book;
+
+@Controller
+@RequestMapping("/book")
+public class BookController {
+
+    private final BookDao bookDao;
+
+    public BookController(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
+
+    @GetMapping(value = "/{id}", produces = "application/json")
+    @ResponseBody
+    public Book book(@PathVariable Long id){
+        return bookDao.findById(id);
+    }
+
+    @PostMapping("")
+    @ResponseBody
+    public Book create(@RequestBody Book book){
+
+        bookDao.save(book);
+
+        return book;
+    }
+
+    @PutMapping("/{id}")
+    @ResponseBody
+    public Book update(@PathVariable Long id, @RequestBody  Book book){
+        Book bookInDb = bookDao.findById(id);
+
+        bookInDb.setTitle(book.getTitle());
+        bookInDb.setRating(book.getRating());
+        bookInDb.setDescription(book.getDescription());
+
+        bookDao.update(book);
+
+        return bookInDb;
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseBody
+    public void delete(@PathVariable Long id){
+        bookDao.remove(id);
+
+    }
+
+}
